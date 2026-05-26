36d5df09a23f08268242495a9b21eb513532a327
R0drigoM1randa <rodrigo.miranda@premium.sv>
2026-05-26 14:21:28 -0600

    Add OAuth request function for GitHub access

---
+++ functions/auth.js
@@ -0,0 +1,9 @@
+export async function onRequest(context) {
+  const { env } = context;
+  const client_id = env.GITHUB_CLIENT_ID;
+  
+  // Solicitamos acceso "repo" para que Decap pueda gestionar los archivos y PRs
+  const url = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo`;
+  
+  return Response.redirect(url, 302);
+}
